21bbc4dfcd8f9262e428a4417394584eabbd1bf4
zxb.zeng <zxb.zeng@shunwang.com>
2025-02-06 16:22:35 +0800

    feat：预览

--- src/views/workFlow/common/form/FormRender.tsx
+++ src/views/workFlow/common/form/FormRender.tsx
@@ -3,6 +3,7 @@ import { Form } from 'ant-design-vue'
 import type { FormInstance } from 'ant-design-vue'
 import FormDesignRender from './formDesignRender'
 import { FormItem } from '../../../../store/modules/design'
+import { useFormSettingsStore } from '@/store/modules/design'
 export default defineComponent({
   name: 'FormRender',
   props: {
@@ -24,15 +25,31 @@ export default defineComponent({
         emit('update:value', val)
       }
     })
+    const formSettingsStore = useFormSettingsStore()
+    // 假设当前是申请人
+    const formPerms = computed(() => {
+      return formSettingsStore.design.process.nodes.find(
+        (node) => node.type === 'apply'
+      )?.properties.formPerms
+    })
     return () => (
       <div>
         <Form ref={formRef} labelCol={{ span: 24 }} wrapperCol={{ span: 24 }}>
           {props.forms.map((el) => {
+            // 根据formPerms判断当前表单项的权限
+            const currentPerm = formPerms.value?.find(
+              (perm) => perm.id === el.id
+            )
+            // 如果没有权限配置或权限为hidden，则不渲染
+            if (!currentPerm || currentPerm.perm === 'hidden') {
+              return null
+            }
+
             return (
               <Form.Item label={el.title} {...el.props}>
                 <FormDesignRender
                   config={el}
-                  mode="EDIT"
+                  mode={currentPerm.perm === 'readonly' ? 'DESIGN' : 'EDIT'}
                   v-model:value={_value.value[el.id]}
                 ></FormDesignRender>
               </Form.Item>
